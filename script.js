2e74c00573cf802593466a3ebeb5bf3c25811be6
Renato Alves <renatoft89@gmail.com>
2021-12-27 18:25:55 -0300

    criando cactus

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+/////////////////////////////// *** FUNÇÂO DINO *** ///////////////////////////
+
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let position = 0;
+let isJumping = false;
 
 function spaceUp(event) {
   if (event.keyCode === 32) {
-    jumping();
+    if (!isJumping) {
+      jumping();
+    }
   }
 }
 
 function jumping() {
-  let position = 0;
+  isJumping = true;
 
   let upMove = setInterval(() => {
     if (position >= 150) {
@@ -17,6 +25,7 @@ function jumping() {
       let downMove = setInterval(() => {
         if (position <= 0) {
           clearInterval(downMove);
+          isJumping = false;
         } else {
           position -= 20;
           dino.style.bottom = position + 'px';
@@ -31,3 +40,36 @@ function jumping() {
 }
 
 document.addEventListener('keyup', spaceUp);
+
+////////////////////////// *** Criar Cactus *** //////////////////////////////
+
+function creatCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let ramdomTime = Math.random() * 10000;
+
+  cactus.classList.add('cactus');
+  cactus.style.left = 1000 + 'px';
+  background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+  cactusPosition -= 10;
+  cactus.style.left = cactusPosition + 'px';
+
+  if (cactusPosition < -60) {
+    clearInterval(leftInterval);
+    background.removeChild(cactus);
+  } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60 ) {
+    // GAME OVER  
+    clearInterval(leftInterval);
+    document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';  } else {  
+    cactusPosition -= 10;
+    cactus.style.left = cactusPosition + 'px';
+  }  
+}, 20);
+
+setTimeout(creatCactus, ramdomTime);
+
+}
+
+creatCactus();
