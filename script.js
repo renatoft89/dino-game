92a932d7d9643aba59299d7e30df87126f18a28f
Renato Alves <renatoft89@gmail.com>
2021-12-27 17:51:36 -0300

    function jump

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const dino = document.querySelector('.dino');
 
 function spaceUp(event) {
-  if(event.keyCode === 32) {
-    console.log('Pressionou Tecla Espaço');
+  if (event.keyCode === 32) {
+    jumping();
   }
+}
+
+function jumping() {
+  let position = 0;
+
+  let upMove = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upMove);
+
+      //Faz o elemento descer
+      let downMove = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downMove);
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      // faz elemento subir
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
 }
 
 document.addEventListener('keyup', spaceUp);
